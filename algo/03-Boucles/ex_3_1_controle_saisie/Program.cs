3be5a81d2fe3de6c9693899fd0b962e7bbbfe1e6
Frederic74200 <120395976+Frederic74200@users.noreply.github.com>
2023-04-28 11:23:02 +0200

    exercices algo pseudo code 3.1 à 3.5

---
+++ algo/03-Boucles/ex_3_1_controle_saisie/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ex_3_1_controle_saisie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
