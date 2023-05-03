eb0a00fd1fcccce75fc189c09f3c10822c4ddf9a
Frederic74200 <120395976+Frederic74200@users.noreply.github.com>
2023-05-03 12:49:13 +0200

    Exercices 4.1 et 4.2 finis

---
+++ algo/4-Tableaux/ex_4_2_lettre_dans_phrase/Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+String phrase ="";
+String point = ".";
+String lettre = ""  ;
+int occurrence = 0;
+int i;
+
+String? saisie;
+bool saisieOK = false;  
+
+
+Console.WriteLine("occurrences d'une lettre dans une phrase.");
+#region
+do
+{
+    Console.WriteLine("saisisez une phrase : ");
+    saisie = Console.ReadLine();
+
+    if (saisie != null)
+    {
+        saisie = saisie + point;
+        phrase = saisie.ToLower();  
+        saisieOK = true;
+    }
+
+} while (!saisieOK);
+#endregion
+
+saisieOK = false;
+saisie = null;
+
+#region
+do
+{
+    Console.WriteLine("Saisissez la lette que vous souhaitez compter : ");
+    saisie = Console.ReadLine();
+
+    if (saisie != null)
+    {
+        lettre = saisie.ToLower();
+        saisieOK = true;
+    }
+
+} while (!saisieOK);
+#endregion
+
+#region
+if (phrase == point || phrase == "..")
+{
+    Console.WriteLine("La phrase est vide");
+}
+
+else
+{
+    for (i= 0; i < phrase.Length; i++)
+    {
+           if (System.String.Compare(phrase[i].ToString(), lettre) == 0 )
+        {
+            occurrence++;
+        }
+    }
+
+    if (occurrence == 0)
+    {
+        Console.WriteLine("La lettre : "+lettre+" n'est pas présente");
+    }
+    else
+    {
+        Console.WriteLine("La lettre : "+lettre+" est présente : "+ occurrence + " fois dans la phrase.");
+    }
+
+}
+#endregion
