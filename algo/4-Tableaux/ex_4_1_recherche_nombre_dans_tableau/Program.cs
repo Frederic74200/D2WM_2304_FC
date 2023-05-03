424a1475ec4b1c92605245b7202f3646609787cf
Frederic74200 <120395976+Frederic74200@users.noreply.github.com>
2023-05-03 12:49:13 +0200

    Exercices 4.1 et 4.2 finis

--- algo/4-Tableaux/ex_4_1_recherche_nombre_dans_tableau/Program.cs
+++ algo/4-Tableaux/ex_4_1_recherche_nombre_dans_tableau/Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+int[] tableau = new int[] {8, 16, 32, 64, 128, 256, 512};
+int nombreN;
+bool nombreTrouve = false;
+int i;
+
+String saisie;
+bool saisieOK;
+
+Console.WriteLine("Le nombre se trouve t-il dans le tableau ?");
+
+do
+{
+    Console.WriteLine("Saisissez un nombre entier : ");
+    saisie = Console.ReadLine();    
+    saisieOK = int.TryParse(saisie, out nombreN );
+    if (saisieOK == false)
+    {
+        Console.WriteLine("Saisie incorrecte ! Recommencez !");
+    }
+} while (!saisieOK);
+
+
+for (i = 0; i < tableau.Length; i++)
+{
+    if (tableau[i] == nombreN)
+    {
+        Console.WriteLine("Le nombre " + nombreN + " se trouve à l'index : " + i); 
+        nombreTrouve = true;    
+    }
+}
+
+
+if (nombreTrouve == false)
+{
+    Console.WriteLine("Le nombre "+nombreTrouve+" n'est pas dans le tableau");
+}
