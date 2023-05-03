b38d85ed5e884f03d7bc2afd0d1ca1c05024e240
Frederic74200 <120395976+Frederic74200@users.noreply.github.com>
2023-05-03 09:00:31 +0200

    coorection partie 3

---
+++ algo/4-Tableaux/04_Tableaux_/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _04_Tableaux_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
