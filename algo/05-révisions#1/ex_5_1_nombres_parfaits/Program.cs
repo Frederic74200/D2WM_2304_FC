93d04dbac67c597ca49b5597f1d091e3f865547f
Frederic74200 <120395976+Frederic74200@users.noreply.github.com>
2023-05-05 10:31:50 +0200

    Exercices jusqu'à 5.1 nombre parfait

---
+++ algo/05-révisions#1/ex_5_1_nombres_parfaits/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] tableauNombresParfaits = new int[4];
+int nombreAdiviser = 2;
+
+
+#region#RemplirTableau
+for (int i = 0; i < tableauNombresParfaits.Length;  i++)
+{
+    bool nombreParfait = false;
+ 
+
+    #region#TrouverUnNombreParfait
+    do
+    {
+        int somme = 0;
+        #region#TrouverAdditionnerLesDiviseurs
+        for (int diviseur = 1; diviseur < nombreAdiviser; diviseur++)
+        {
+            if (nombreAdiviser % diviseur == 0)
+            {
+                somme = somme + diviseur;
+            }
+        }
+        #endregion#TrouverAdditionnerLesDiviseurs
+
+        #region#VerifierSiSomme=Nomnre
+        if (nombreAdiviser == somme)
+        {
+            nombreParfait = true;
+        }
+        else
+        {
+            nombreAdiviser++;
+        }
+        #endregion#VerifierSiSomme=Nomnre
+    } while (!nombreParfait);
+    #endregion#TrouverUnNombreParfait
+
+    if (nombreParfait == true)
+    {
+        tableauNombresParfaits[i] = nombreAdiviser;
+    }
+
+    nombreAdiviser++;
+}
+#endregion#RemplirTableau
+
+
+#region#AfficherTableau
+Console.WriteLine("Les 4 premiers nombres parfaits sont : ");
+foreach(int i in tableauNombresParfaits)
+{
+    Console.Write("{0} ", i);
+}
+
+#endregion#AfficherTableau
+
+
+
+
---
+++ algo/4-Tableaux/04_Tableaux_/ex_4_4_Bis_Tri_tableau/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int[] tableau = new int[7] { 128, 64, 8, 512, 16, 32, 256 };
+
+// afficher tableau départ
+Console.WriteLine("Tableau initial : \n\r");
+foreach (int i in tableau)
+{
+    Console.Write("{0} ", i);
+}
+
+
+#region#GrandeBoucleFor
+for (int i= 0; i< tableau.Length; i++)
+{
+    int tmp = tableau[i];
+    int min =0;
+    Console.WriteLine("tmp 1  "+tmp+"  ");
+
+    for (int j= i; j< tableau.Length; j++)
+    {
+        min = tableau[j];   
+        if (tableau[j] < min)
+        {
+            min = tableau[j];
+        }
+    }
+    Console.WriteLine("tmp 2 min 2 " + tmp + "  " + min);
+    tableau[i] = min;
+
+    for (int j= i +1; j< tableau.Length; j++)
+    {
+        if (tableau[j] == min) {
+            tableau[i] = tmp;
+        }
+    }
+    Console.WriteLine("tmp 3 min 3 " + tmp + "  " + min);
+}
+#endregion#GrandeBoucleFor
+
+
+// afficher tableau final
+Console.WriteLine("\n\r Tableau final : \n\r");
+
+foreach (int i in tableau)
+{
+    Console.Write("{0} ", i);
+}
+
+
