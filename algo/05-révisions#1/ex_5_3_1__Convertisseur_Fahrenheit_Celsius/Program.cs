339026fa242e7f3cb5c19714e4b49d3bb53e486a
Frederic74200 <120395976+Frederic74200@users.noreply.github.com>
2023-05-10 15:43:38 +0200

    Exercice 5.3.1 convertisseur F C + changement nom dossier 04

---
+++ algo/05-révisions#1/ex_5_3_1__Convertisseur_Fahrenheit_Celsius/Program.cs
@@ -0,0 +1,136 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+CultureInfo culture1 = CultureInfo.CurrentCulture;
+
+const double MINFAHRENHEIT = -459.67;
+const double MINCELSIUS = -273.15;
+const double MAX = 5000000d;
+string quit;
+
+#region#quit
+do
+{
+    String? saisie;
+    bool saisieOK = false;
+    String farOUcel = "";
+    double nombreAconcertir = 0d;
+    double nombreConverti;
+
+    #region#VerifSaise
+    do
+    {
+        Console.WriteLine("Converstissuer Fahrenheit Celsius");
+        Console.WriteLine("\t Saisissez la valeur à convertir puis son unité précédée d'un espace. \n\r" +
+                          "\t F pour Fahrenheit | C pour Celsius \n\r" +
+                          "\t Valeur minimum en degrés Fahrenheit : -456,67 F \n\r" +
+                          "\t Valeur minimum en degrés Celsius : -273,15 C ");
+        saisie = Console.ReadLine().ToLower();
+
+        #region#VerifSaisieNull
+        if (saisie != null)
+        {
+            #region#Extration2derniersCaracteres
+            for (int i = saisie.Length - 2; i < saisie.Length; i++)
+            {
+                farOUcel = farOUcel + saisie[i];
+            }
+            #endregion#Extration2derniersCaracteres
+
+
+            #region#verifFarouCel
+            int indexSaisie = saisie.Length;
+
+            if (farOUcel == " f" || farOUcel == " c")
+            {
+                saisie = saisie.Remove(indexSaisie - 1);
+                saisie = saisie.Remove(indexSaisie - 2);
+            }
+            else
+            {
+                saisieOK = false;
+                Console.WriteLine("Erreur Saisie !");
+            }
+            #endregion#verifFarouCel
+
+
+            #region#VerifConversionDouble
+            try
+            {
+                nombreAconcertir = double.Parse(saisie, culture1);
+            }
+            catch (Exception e)
+            {
+                saisieOK = false;
+                Console.WriteLine("Erreur Saisie !");
+            }
+            #endregion#VerifConversionDouble
+
+
+            #region#VerifMinMax
+            switch (farOUcel)
+            {
+                case " f":
+                    if (nombreAconcertir < MINFAHRENHEIT || nombreAconcertir > MAX)
+                    {
+                        saisieOK = false;
+                        Console.WriteLine("Erreur Saisie !");
+                    }
+                    else
+                    {
+                        saisieOK = true;
+                    }
+                    break;
+
+                case " c":
+                    if (nombreAconcertir < MINCELSIUS || nombreAconcertir > MAX)
+                    {
+                        saisieOK = false;
+                        Console.WriteLine("Erreur Saisie !");
+                    }
+                    else
+                    {
+                        saisieOK = true;
+                    }
+                    break;
+            }
+            #endregion#VerifMinMax
+
+        }
+        else
+        {
+            saisieOK = false;
+            Console.WriteLine("Erreur Saisie !");
+        }
+        #endregion#VerifSaisieNull
+
+    } while (!saisieOK);
+    #endregion#VerifSaise
+
+
+    #region#Conversion
+    switch (farOUcel)
+    {
+        case " f":
+            nombreConverti = (nombreAconcertir - 32) * 5 / 9;
+            Console.WriteLine(nombreAconcertir + " °F correspondent à : " + nombreConverti + " °C.");
+            break;
+
+        case " c":
+            nombreConverti = (nombreAconcertir * 9 / 5) + 32;
+            Console.WriteLine(nombreAconcertir + " °C correspondent à : " + nombreConverti + " °F.");
+            break;
+    }
+    #endregion#Conversion
+
+    #region#SaisieQuit
+    Console.WriteLine("Taper q pour quitter ou sur une autre touche pour remmencer.");
+    quit = Console.ReadLine().ToLower();
+    #endregion#SaisieQuit
+
+} while (quit != "q");
+#endregion#quit
+
+
+
+
--- algo/05-révisions#1/ex_9_9_test/Program.cs
+++ algo/05-révisions#1/ex_9_9_test/Program.cs
@@ -2,73 +2,23 @@
 
 // test
 
- const double MILESKM = 1.609;
-string quit;
 
-do
+
+using System.Globalization;
+using System.Text;
+
+CultureInfo culture1 = CultureInfo.CurrentCulture;
+
+String value = "310,16";
+try
 {
-
-    String saisie;
-    String nombreSaisie = "";
-    String kmOUmi = "";
-
-    double nombreAconcertir;
-    double nombreConverti;
-
-    do
-    {
-
-        Console.WriteLine("Converstissuer kilomètres miles");
-        Console.WriteLine("Saisissez la valeur à convertir, inférieure à 1000000, et son unité (km pour kilomètres ou mi pour miles");
-        saisie = Console.ReadLine().ToLower();
-
-        for (int i = 0; i < saisie.Length; i++)
-
-        {
-            if (char.IsNumber(saisie[i]) || saisie[i] == 46)
-            {
-                nombreSaisie = nombreSaisie + saisie[i];
-            }
-            else
-            {
-                kmOUmi = kmOUmi + saisie[i];
-            }
-        }
-
-        nombreAconcertir = Convert.ToDouble(nombreSaisie);
-
-
-        Console.WriteLine(nombreAconcertir + "   " + kmOUmi);
-
-
-
-    } while (nombreAconcertir < 0.01 || nombreAconcertir > 1000000d || saisie == null);
-
-
-    switch (kmOUmi)
-    {
-        case "km":
-            nombreConverti = nombreAconcertir / MILESKM;
-            Console.WriteLine(nombreAconcertir+" kilomètres équivalent à :"+nombreConverti+" miles.");
-            break;
-
-        case "mi":
-            nombreConverti = nombreAconcertir * MILESKM;
-            Console.WriteLine(nombreAconcertir + " miles équivalent à :" + nombreConverti + " kilomètres.");
-            break;
-        case null:
-            nombreConverti = nombreAconcertir / MILESKM;
-            Console.WriteLine(nombreAconcertir + " équivalent à :" + nombreConverti + " milse.");
-            break;
-        default:
-            Console.WriteLine("Saisie érronnée !");
-            break;
-
-    }
-
-
-
-    Console.WriteLine("Taper q pour quitter ou sur une autre touche pour remmencer.");
-    quit = Console.ReadLine().ToLower(); 
-
-} while (quit != "q");
+    Console.WriteLine("{0} culture reflects user overrides: {1}",
+                      culture1.Name, culture1.UseUserOverride);
+    Decimal amount = Decimal.Parse(value, culture1);
+    Console.WriteLine("'{0}' --> {1}", value, amount.ToString(CultureInfo.InvariantCulture));
+}
+catch (FormatException)
+{
+    Console.WriteLine("Unable to parse '{0}'", value);
+}
+Console.WriteLine();
