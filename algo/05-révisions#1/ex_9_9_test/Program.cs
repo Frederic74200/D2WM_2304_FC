1087970e126d624ad62419194ae36f3b233a8dc6
Frederic74200 <120395976+Frederic74200@users.noreply.github.com>
2023-05-11 09:47:53 +0200

    exercie 5.3.2 fin partie 5

--- algo/05-révisions#1/ex_9_9_test/Program.cs
+++ algo/05-révisions#1/ex_9_9_test/Program.cs
@@ -9,16 +9,20 @@ using System.Text;
 
 CultureInfo culture1 = CultureInfo.CurrentCulture;
 
-String value = "310,16";
-try
-{
-    Console.WriteLine("{0} culture reflects user overrides: {1}",
-                      culture1.Name, culture1.UseUserOverride);
-    Decimal amount = Decimal.Parse(value, culture1);
-    Console.WriteLine("'{0}' --> {1}", value, amount.ToString(CultureInfo.InvariantCulture));
-}
-catch (FormatException)
-{
-    Console.WriteLine("Unable to parse '{0}'", value);
-}
-Console.WriteLine();
+double value = 15.4;
+
+
+
+double test = Math.Ceiling(value); // celling -< supp
+Console.WriteLine("test  : " + test);
+
+int test2 = Convert.ToInt32(test);
+
+Console.WriteLine("test 2 : " + test2);
+
+double tesmin = Math.Floor(value);
+Console.WriteLine("test min : " + tesmin);
+
+int test3 = Convert.ToInt32(tesmin);
+Console.WriteLine("test 3 : " + test3);
+
---
+++ algo/06-révisions#2/ex_9_9_test/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ex_9_9_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<string> legumes = new List<string>();
+            List<int> tableau2 = new List<int>();
+
+
+            tableau1.Add(tableauDepart[i]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
