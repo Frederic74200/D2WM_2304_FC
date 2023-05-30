81acc1d5dd033614f95d5086d696046cc0b43e74
Frederic74200 <120395976+Frederic74200@users.noreply.github.com>
2023-05-30 10:44:35 +0200

    début objet pocédures/fonctions

---
+++ Objet/Decouverte/ProcedureFonction/ProcedureFonction.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcedureFonction
+{
+    internal class ProcedureFonction
+    {
+
+        // attributs  ------------------
+        double nb1;
+        double nb2;
+        string prenom;
+
+
+
+        // constructeurs -------------------
+
+        // par défaut 
+        public ProcedureFonction()
+        {
+            this.nb1 = 0.01;
+           this.nb2 = 0.0001;
+           this.prenom = "Fred";
+        }
+
+        // avec paramètres 
+
+        public ProcedureFonction(double _nb1, double _nb2, string _prenom)
+        {
+            this.nb1 = _nb1;    
+            this.nb2 = _nb2;
+            this.prenom = _prenom;
+        }
+
+
+
+        // fonctions ---------------------------
+
+
+        public double AdditionnerDeuxNombresReels()
+
+        {
+            double somme = nb1 + nb2;
+
+            return somme;
+        }
+
+        public void AfficherBonjourEtPrenom()
+        {
+            Console.WriteLine("Bonjour "+this.prenom);
+        }
+
+
+    }
+}
---
+++ Objet/Decouverte/ProcedureFonction/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ProcedureFonction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProcedureFonction prfc1 = new ProcedureFonction();
+            ProcedureFonction prfc2 = new ProcedureFonction(3.14, 44.045, "Lilly");
+
+            Console.WriteLine("Somme 1 : " + prfc1.AdditionnerDeuxNombresReels());
+            Console.WriteLine("Somme 2 : " + prfc2.AdditionnerDeuxNombresReels());
+            prfc1.AfficherBonjourEtPrenom();
+            prfc2.AfficherBonjourEtPrenom();    
+
+
+
+
+        }
+    }
+}
