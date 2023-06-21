a660aa044eb45399cf9411c289218bc56a899cb3
Frederic74200 <120395976+Frederic74200@users.noreply.github.com>
2023-06-21 16:51:37 +0200

    Fin voiture avant correction

---
+++ Objet/09-Voiture/C#/ClassLibraryVoiture/Roue.cs
@@ -0,0 +1,71 @@
+﻿namespace ClassLibraryVoiture
+{
+    public class Roue
+    {
+
+
+        // Attributs ------------------------------------------------------------------------
+        private string dimensionPneu;
+
+        private bool tourne;
+
+        // get set ----------------------------------------------------------------------------
+        public bool Tourne { get => tourne; set => tourne = value; }
+
+
+
+        // Constructeurs ------------------------------------------------------------------
+
+        public Roue(bool _tourne)
+        {
+            this.tourne = _tourne;
+        }
+
+        public Roue():this(false) { }
+
+        public Roue(Roue roueAcopier) :this(roueAcopier.Tourne) { } 
+
+
+
+        // Fonctions ------------------------------------------------------------------------
+
+        // Tourner
+        public bool Tourner()
+        {
+            if(!tourne)
+            {
+                tourne = true;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        // stopper
+        public bool Stopper()
+        {
+            if (tourne)
+            {
+                tourne = false;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // roue to string 
+         public string roueToString()
+        {
+            string roueToString = "Roue tourne ? " + Tourne+"\n\r";
+            return roueToString;
+        }
+
+
+
+    }
+}
---
+++ Objet/09-Voiture/C#/ConsoleAppVoiture/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibraryVoiture;
+
+Console.WriteLine(" V1 Etat initial!");
+
+Voiture v1 = new Voiture();
+Console.WriteLine(v1.voitureToString());
+v1.DemarrerLeMoteur();
+v1.Avancer();
+
+Console.WriteLine(" V1 Demarrer moteur, Avancer!");
+Console.WriteLine(v1.voitureToString());
+
+Console.WriteLine("V1 Freiner!");
+v1.Freiner();
+Console.WriteLine(v1.voitureToString());
+
+
+// -------------------------------
+
+
+Console.WriteLine("Hello, World!");
+
+// Voiture v2 = new Voiture(new Moteur(true), );
