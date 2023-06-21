e71814c959fb5c913b7a27b1b4782f824e8d11d7
Frederic74200 <120395976+Frederic74200@users.noreply.github.com>
2023-06-21 16:51:37 +0200

    Fin voiture avant correction

---
+++ Objet/09-Voiture/C#/ClassLibraryVoiture/Moteur.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryVoiture
+{
+    public class Moteur
+    {
+        // Attributs -----------------------------------------------------------
+        private bool enMarche;
+
+        // get set-----------------------------------------------------------------
+        public bool EnMarche { get => enMarche; set => enMarche = value; }
+
+
+        // Constructeurs -------------------------------------------------------------------
+        public Moteur(bool _enMarche)
+        {
+            this.enMarche = _enMarche;
+        }
+
+        public Moteur() : this(false) { }
+
+        public Moteur(Moteur moteurAcopier) : this(moteurAcopier.EnMarche) { }
+
+
+        // Fonctions ---------------------------------------------------------------------
+
+        // Demmarer
+        public bool Demarrer()
+        {
+            if (!enMarche)
+            {
+                enMarche = true;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Entrainer roue
+        public bool EntrainerRoues(Roue roue2, Roue roue1)
+        {
+            if (enMarche && roue2.Tourner() == true &&  roue1.Tourner() == true)
+            {
+
+                roue2.Tourner();
+                roue1.Tourner(); 
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Arreter roues 
+        public bool ArreterRoues(Roue roue1, Roue roue2)
+        {
+            if (enMarche && roue1.Stopper() == true && roue2.Stopper() == true)            
+            {
+                roue1.Stopper();
+                roue2.Stopper();
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // eteindre 
+        public bool Eteindre()
+        {
+            if (enMarche)
+            {
+                enMarche = false;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // moteur to string 
+
+        public string moteurToString()
+        {
+            string moteurToString = "moteur enMarche ? " + enMarche + "\n\r";
+            return moteurToString;
+        }
+
+
+    }
+}
---
+++ Objet/09-Voiture/C#/ClassLibraryVoiture/Voiture.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLibraryVoiture
+{
+    public class Voiture
+    {
+        // Attributs --------------------------------------------------------------------------------
+
+        private string marque;
+        private Moteur moteur;
+        private List<Roue> roues;
+
+        public string Marque { get => marque; set => marque = value; }
+
+
+        // Constructeurs ---------------------------------------------------------------------------
+        public Voiture(Moteur _moteur, List<Roue> _roues, string _marque)
+        {
+            moteur = _moteur;
+            roues = _roues; 
+            marque = _marque;
+        }
+
+
+
+        public Voiture(Voiture voitureAcopier)
+            : this(new Moteur(voitureAcopier.moteur),
+                 new List<Roue>(){new Roue(voitureAcopier.roues[0]),
+                                new Roue(voitureAcopier.roues[1]),
+                                new Roue(voitureAcopier.roues[2]),
+                                new Roue(voitureAcopier.roues[3])},
+                 voitureAcopier.marque)
+        {
+
+        }
+
+        public Voiture()
+           : this(new Moteur(), new List<Roue> { new Roue(), new Roue(), new Roue(), new Roue() }, "Peugeot")
+        {
+
+        }
+
+        // Fonctions --------------------------------------------------------------------------------
+
+        // Démarre 
+        public bool DemarrerLeMoteur()
+        {
+
+            if (moteur.Demarrer() == true) { return true; }
+            else { return false; }
+
+        }
+
+        // Avancer
+        public bool Avancer()
+        {
+
+            if ( moteur.EntrainerRoues(roues[0], roues[1]) == true)
+            {
+                moteur.EntrainerRoues(roues[0], roues[1]);
+                roues[2].Tourner();
+                roues[3].Tourner();
+
+                return true;
+            }
+            else { return false; }
+
+        }
+
+        // Freiner 
+
+        /*/
+        public bool Freiner()
+        {
+            if (roues[0].Tourne == true || roues[1].Tourne == true || roues[2].Tourne == true || roues[3].Tourne == true)
+            {
+                for (int i = 0; i < roues.Count; i++) { roues[i].Stopper(); }
+                return true;
+            }
+            else { return false; }
+
+        }
+        */
+        public bool Freiner()
+        {
+
+            if (moteur.ArreterRoues(roues[0], roues[1]) == true)
+            {
+                moteur.ArreterRoues(roues[0], roues[1]);
+                roues[2].Stopper();
+                roues[3].Stopper();
+
+                return true;
+            }
+            else { return false; }
+
+        }
+
+
+
+
+        // voiture to string 
+
+        public string voitureToString()
+        {
+            string voitureToString = "-----------------------------------------------\n\r" +
+                                    "Marque Voiture : " + marque + "\n\r" +
+                                    moteur.moteurToString() +
+                                    roues[0].roueToString() +
+                                     roues[1].roueToString() +
+                                     roues[2].roueToString() +
+                                      roues[3].roueToString() +
+                                     "-----------------------------------------------\n\r";
+            return voitureToString;
+        }
+
+
+    }
+}
