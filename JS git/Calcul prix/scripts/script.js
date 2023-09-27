560142e74b1c05912eb1d940680ebe39ddc2f827
Frederic74200 <120395976+Frederic74200@users.noreply.github.com>
2023-09-27 16:58:07 +0200

    formulaire JS

--- JS git/Calcul prix/scripts/script.js
+++ JS git/Calcul prix/scripts/script.js
@@ -1,119 +1,27 @@
-console.log("toto");
-
-/*
-let tableau = [];
-tableau = document.querySelectorAll('.remp');
-
-for (let el in tableau) {
-    tableau[el].placeholder = 'votre message ici !';
-}
-
-
-for (let i = 0; i < tableau.length; i++) {
-    tableau[i].placeholder = 'Votre message ici ! ';
-}
 
 
 
-tableau.forEach(el => el.placeholder = "votre message : ");
+function calculerPrix() {
 
-  <input type="text" name="" id="monTxt1" class="remp">
-        <input type="text" name="" id="monTxt2" class="remp">
-        <input type="text" name="" id="monTxt3" class="remp">
-        <input type="text" name="" id="monTxt4" class="remp">
-        <input type="text" name="monTxt5" id="monTxt6" class="remp">
-        <input type="text" name="" id="monTxt6" class="remp">
+    let q1 = document.querySelector('#q1').value;
+    let q2 = document.querySelector('#q2').value;
+    let pu1 = document.querySelector('#pu1').value;
+    let pu2 = document.querySelector('#pu2').value;
 
+    (isNaN(q1) && q1 < 0 ? alert('Saisie incorrecte !') : '');
+    (isNaN(q2) && q2 < 0 ? alert('Saisie incorrecte !') : '');
+    (isNaN(pu1) && pu1 < 0 ? alert('Saisie incorrecte !') : '');
+    (isNaN(pu2) && pu2 < 0 ? alert('Saisie incorrecte !') : '');
 
-/*/
+    let px1 = q1 * pu1;
+    let px2 = q2 * pu2;
 
-
-
-function tableauPrix() {
-
-
-    // créer imput quantité 
-
-    let inputQ = document.createElement('input');
-    inputQ.type = 'number';
-    inputQ.addEventListener('change', getQ());
-    let labelQuant = document.createElement('label');
-    let textQ = document.createTextNode("Quantité : ");
-    labelQuant.appendChild(textQ);
-    let tdQ = document.createElement('td');
-    tdQ.appendChild(labelQuant);
-    tdQ.append(inputQ);
-
-    // créer imput Prix unitare  
-
-    let inputPU = document.createElement('input');
-    inputPU.type = 'number';
-    let labelPU = document.createElement('label');
-    let textPU = document.createTextNode("Prix unitaire : ");
-    labelPU.appendChild(textPU);
-    let tdPU = document.createElement('td');
-    tdPU.appendChild(labelPU);
-    tdPU.append(inputPU);
-
-
-    // créer imput Prix   
-
-    let inputPrx = document.createElement('input');
-    inputPrx.type = 'number';
-    let labelPrx = document.createElement('label');
-    let textPrx = document.createTextNode("Prix : ");
-    labelPrx.appendChild(textPrx);
-    let tdPrx = document.createElement('td');
-    tdPrx.appendChild(labelPrx);
-    tdPrx.append(inputPrx);
-
-
-    // créer imput total   
-
-    let inputTl = document.createElement('input');
-    inputTl.type = 'number';
-    let labelTl = document.createElement('label');
-    let textTl = document.createTextNode("Total : ");
-    labelTl.appendChild(textTl);
-    let tdTl = document.createElement('td');
-    tdTl.appendChild(labelTl);
-    tdTl.append(inputTl);
-
-
-
-    // constituer une ligne Q pu 
-
-    let ligneCalcul = document.createElement('tr');
-    ligneCalcul.appendChild(tdQ);
-    ligneCalcul.appendChild(tdPU);
-    ligneCalcul.appendChild(tdPrx);
-
-    // constituer ligne total 
-    let ligneTl = document.createElement('tr');
-    ligneTl.appendChild(tdTl);
-
-    let tbody = document.createElement('tbody');
-
-
-
-
-    // insérer plusieurs lignes q pu dans tbody  
-
-
-    for (let i = 0; i < 12; i++) {
-
-        tbody.appendChild(ligneCalcul.cloneNode(true));
-    }
-
-    document.querySelector('#tableau').appendChild(tbody);
-
-    document.querySelector('#tableau').append(ligneTl);
+    document.querySelector('#px1').value = px1;
+    document.querySelector('#px2').value = px2;
+    document.querySelector('#total').value = px1 + px2;
 
 }
-tableauPrix();
-
-function getQ() {
-    
-}
-
-
+document.querySelector('#q1').addEventListener('input', calculerPrix);
+document.querySelector('#q2').addEventListener('input', calculerPrix);
+document.querySelector('#pu1').addEventListener('input', calculerPrix);
+document.querySelector('#pu2').addEventListener('input', calculerPrix);
---
+++ JS git/Code hexa couleur/js/script.js
@@ -0,0 +1,70 @@
+console.log("toto");
+
+// Contrôler la saisie ----------------------------------------- 
+function validInput(inputColor) {
+
+    let control = false;
+
+    let hexa = document.querySelector('#' + inputColor).value;
+
+    if (hexa.match(new RegExp("^[A-Fa-f0-9]{2}$"))) {
+        console.log('saisie ok');
+        control = true;
+    }
+    else {
+        console.log('erreur !');
+        document.querySelector('#' + inputColor).value = '';
+        document.querySelector('#' + inputColor).focus();
+    }
+
+}
+
+// chnager la couleur du body en fonction de la saisie 
+function changeColor() {
+    let rg = document.querySelector('#hexa_rg').value.toString();
+    let vrt = document.querySelector('#hexa_vrt').value.toString();
+    let bl = document.querySelector('#hexa_bl').value.toString();
+    let color = '#' + rg + vrt + bl;
+    document.body.style.backgroundColor = color;
+}
+
+// passer le body en rouge en cliquant sur le bouton
+function rouge() {
+    document.body.style.backgroundColor = '#FF0000';
+}
+
+// passer le body en vert en cliquant sur le bouton
+
+function vert() {
+    document.body.style.backgroundColor = '#00FF00';
+}
+// passer le body en bleu en cliquant sur le bouton
+
+function bleu() {
+    document.body.style.backgroundColor = '#0000ff';
+}
+
+// inverser color body et h1
+
+function invertColor() {
+    let elmBody = document.querySelector('body');
+    let color = elmBody.style.backgroundColor;
+    console.log(color);
+
+    let titre = document.querySelector('h1');
+    titre.style.color = color;
+    titre.style.filter = 'invert(100%)';
+}
+
+
+// associer les contôles de siasie et changement de couleur body aux input et changement couleur h1
+document.querySelector('#hexa_rg').addEventListener('blur', function () { validInput('hexa_rg'); changeColor(); invertColor(); });
+document.querySelector('#hexa_vrt').addEventListener('blur', function () { validInput('hexa_vrt'); changeColor(); invertColor(); });
+document.querySelector('#hexa_bl').addEventListener('blur', function () { validInput('hexa_bl'); changeColor(); invertColor(); });
+
+// associer changement couleurs aux boutons  et changement couleur h1
+document.querySelector('#btn_rg').addEventListener('click', function () { rouge(); invertColor() });
+document.querySelector('#btn_vrt').addEventListener('click', function () { vert(); invertColor() });
+document.querySelector('#btn_bl').addEventListener('click', function () { bleu(); invertColor() });
+
+
