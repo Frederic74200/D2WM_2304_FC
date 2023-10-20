db9fd0a54bfbf69ffdb222a0858f678d469f5370
Frederic74200 <120395976+Frederic74200@users.noreply.github.com>
2023-10-20 10:58:22 +0200

    suite vue js

--- Vue js/Code postaux/asserts/js/Recherche.js
+++ Vue js/Code postaux/asserts/js/Recherche.js
@@ -1,16 +1,17 @@
 class Recherche {
 
     rechercheCP(_input, _cpJson, _villeJson) {
-        if (_input.length > 1) {
 
-            for (let i = 0; i < _input.length; i++) {
 
-                if (_cpJson.indexOf(_input) < -1) {
-                    return _villeJson;
-                }
-            }
-        }
+        const codePostal = _input.slice(0, 2);
+
+        const villes = _villeJson.filter((ville) => ville.codePostal === codePostal);
+
+        return villes;
     }
 }
+
+
+
 
 export { Recherche }; 
